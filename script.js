2e5082514378cdf85f0801122965c0b8f3c782c8
AHMEDALBREEM <amealbrem@gmail.com>
2025-01-11 03:16:03 +0600

    1

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleContactInfo() {
+    const contactInfo = document.getElementById('contactInfo');
+    if (contactInfo.style.display === 'none') {
+      contactInfo.style.display = 'block';
+    } else {
+      contactInfo.style.display = 'none';
+    }
+  }
+  
