9f42c63b17509b897a1a5d96c7a5800272cdace3
AHMED M. S. ALBREEM <149643278+AHMEDALBREEM@users.noreply.github.com>
2025-01-18 05:07:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleContactInfo() {
 
 let timeoutId;
 let currentIndex = 0;
-const maxIndex = 31;
+const maxIndex = 32;
 
 function showImage(index) {
     const images = document.querySelectorAll('.certificate-img');
